5033d195387e47af2fb10699fdf9f939fb32bda9
BintangDiLangit <supercellbanned1@gmail.com>
2024-11-21 15:18:16 +0700

    telegram finishing

--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta property="og:title" content="{{ $url->title }}">
-    <meta property="og:image" content="{{ asset('storage/' . $item->banner_image) }}">
+    <meta property="og:image" content="{{ asset('storage/' . $url->banner_image) }}">
     <meta http-equiv="refresh" content="1;url={{ $url->url }}">
     <title>Redirecting...</title>
 </head>
